df11b7c4c57a93fd803464ce05ee43dd607c250b
swanpark8538 <swanpark8538@gmail.com>
2026-03-11 16:40:49 +0900

    archiver 의존성 삭제

--- src/utils/projectGenerator.ts
+++ src/utils/projectGenerator.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode"
 import * as fs from "fs-extra"
 import * as path from "path"
-import archiver from "archiver"
 import extractZip from "extract-zip"
 
 export interface EgovProjectTemplate {
